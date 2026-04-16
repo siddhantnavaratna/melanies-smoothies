588c114ac73cb58ea8cbdade0d903287a21d2291
siddhantnavaratna <56848142+siddhantnavaratna@users.noreply.github.com>
2026-04-16 12:38:36 -0400

    Update streamlit_app.py
    adding REST api calls

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ if ingredients_list:
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
                     values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
 
-    st.write(my_insert_stmt)
+    #st.write(my_insert_stmt)
     #st.stop()
     time_to_insert = st.button("Submit Order")
 
@@ -42,6 +42,9 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success("""Your Smoothie is ordered,"""+name_on_order+"""!""", icon="✅")
 
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
 
 
 
